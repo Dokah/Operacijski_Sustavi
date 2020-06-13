beb8fc690eb6d6e0fffb38800cd799ce6e07eb15
Dominik_K <dodo3334@gmail.com>
2020-06-13 17:42:01 +0200

    Prvi dio cetvrtoga zadatka ali jos dretve zezaju

--- Cetvrti_Zadatak/Cetvrti_a.cpp
+++ Cetvrti_Zadatak/Cetvrti_a.cpp
@@ -1,32 +1,68 @@
 #include<iostream>
 #include<cstdlib>
+#include<stdlib.h>
+#include<unistd.h>
 #include<pthread.h>
+#include<signal.h>
+#include<sys/types.h>
 using namespace std;
 
-int N;
+pthread_mutex_t m;
+pthread_cond_t red;
+
+int N,br;
 int *ZajednickiProstor=new int [N];
 
-void *Upisi(void *arg){
+pthread_t *thr_id=new pthread_t [N];
+
+void brisi(){
+ cout<<"Brisem"<<endl;
+ for(int i=0;i<N;i++)pthread_join(thr_id[i],NULL);
+ pthread_mutex_destroy(&m);
+ pthread_cond_destroy(&red);
+}
+
+void brisi(int sig){
+ cout<<"Brisem"<<endl;
+ for(int i=0;i<N;i++)pthread_join(thr_id[i],NULL);
+ pthread_mutex_destroy(&m);
+ pthread_cond_destroy(&red);
+}
+
+void *barijera(void *arg){
+ pthread_mutex_lock(&m);
  int broj;
  int index=*((int *)arg);
  cout<<"Dretva "<<index<<". "<<"unesite broj: "<<endl;
  cin>>broj;
+ br++;
+ if(br<N){
+  pthread_cond_wait(&red, &m);
+ }
+ else{
+  br=0;
+  pthread_cond_signal(&red);
+ }
  cout<<"Dretva "<<index<<". "<<"uneseni broj je: "<<broj<<endl;
+ pthread_mutex_unlock(&m);
  pthread_exit(NULL);
 }
 
 int main(int argc,char** argv){
+ sigset(SIGINT,brisi);
  N=atoi(argv[1]);
- pthread_t thr_id[N];
+
+ pthread_mutex_init(&m, NULL);
+ pthread_cond_init(&red, NULL);
 
  for(int i=0;i<N;i++){
-  if (pthread_create(&thr_id[i], NULL, Upisi, &i) != 0) {
+  if (pthread_create(&thr_id[i], NULL, barijera, &i) != 0) {
    printf("Greska pri stvaranju dretve!\n");
    exit(1);
   }
  }
 
- for(int i=0;i<N;i++)pthread_join(thr_id[i],NULL);
+ brisi();
 
  return 0;
 }
